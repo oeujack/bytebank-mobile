5222db7541939d3f7452aa4867e80d492ce15afc
Raul Lima <raul.ferreira.2@hotmail.com>
2025-09-21 19:05:22 -0300

    Adicionando componente de dashboard e fazendo alguns ajustes nos componentes

---
+++ mobile/src/assets/mock/dashboarMock.ts
@@ -0,0 +1,19 @@
+
+export const expensesByCategory = [
+  { x: 'Alimentação', y: 1200 },
+  { x: 'Transporte', y: 450 },
+  { x: 'Estudos', y: 600 },
+  { x: 'Contas', y: 950 },
+  { x: 'Investimento', y: 500 },
+  { x: 'Reserva', y: 500 },
+];
+
+
+export const recentTransactions = {
+  labels: ["iFood", "Uber", "Mercado", "Cinema"], 
+  datasets: [
+    {
+      data: [85, 42, 370, 60], 
+    },
+  ],
+};
