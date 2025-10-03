1f1ebb95136825795051c38bca3cb155aced2152
thmsaguiar <thvicente110100@gmail.com>
2025-10-03 07:43:02 -0300

    fix: ajuste no campo valor ao editar transação de transferência

--- mobile/src/screens/AddEditTransaction.tsx
+++ mobile/src/screens/AddEditTransaction.tsx
@@ -64,7 +64,7 @@ export function AddEditTransaction() {
       if (transaction) {
         setAccountType(transaction.account_type);
         setTransactionType(transaction.transaction_type);
-        setAmount(transaction.amount.toString());
+        setAmount(Math.abs(transaction.amount).toString());
         setDescription(transaction.description || "");
         setAttachmentUrl(transaction.attachment_url || null);
       }
